5015b0f31d40409eda5a03072a8f4c96c79468b3
Gretar Mar <grellinn@gmail.com>
2016-10-30 16:25:12 +0000

    TicTacToe.java now initializes multidimensional array from 1 - 9

--- src/main/java/is/gens/tictactoe/TicTacToe.java
+++ src/main/java/is/gens/tictactoe/TicTacToe.java
@@ -2,7 +2,7 @@ package is.gens.tictactoe;
 
 public class TicTacToe {
     public static void main(String[] args) {
-		char[][] table = new char[3][3];
+		char[][] table = {{'1', '2', '3'}, {'4', '5', '6'}, {'7', '8', '9'}};
 		
 		Board board = new Board();    
 		board.displayBoard(table);
