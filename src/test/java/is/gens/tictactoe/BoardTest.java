0c6e79e1a1f93a841cce340e3e1059dcc0605b94
Ragnarstefanss <ragnars15@ru.is>
2016-10-30 19:03:32 +0000

    Test for X in row works

--- src/test/java/is/gens/tictactoe/BoardTest.java
+++ src/test/java/is/gens/tictactoe/BoardTest.java
@@ -1,6 +1,6 @@
 package is.gens.tictactoe;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 import org.junit.Test;
 
@@ -8,9 +8,23 @@ public class BoardTest {
 
     @Test
     public void displayEmptyBoard() {
-		char[][] table = new char[3][3];
-        Board board = new Board();
+		char[][] table = {{'1', '2', '3'}, {'4', '5', '6'}, {'7', '8', '9'}};
+		
+		Board board = new Board();
 		
         assertEquals("Everything is gonna be okay!", board.displayBoard(table)); 
 	}
+	
+	@Test
+	public void checkWinnerRow() {
+
+    	char[][] table = {{'X', 'X', 'X'}, {'4', '5', '6'}, {'7', '8', '9'}};
+    	
+    	Board board = new Board();
+    	boolean check = board.checkRowWinner(table);
+
+    	assertTrue(board.checkRowWinner(table));
+	 }
+
+
 }
