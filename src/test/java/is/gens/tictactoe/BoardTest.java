3a0a23e6efa09e52b7974fc70b805cb69433137f
Ragnarstefanss <ragnars15@ru.is>
2016-10-30 20:29:50 +0000

    Addded test for canMove function

--- src/test/java/is/gens/tictactoe/BoardTest.java
+++ src/test/java/is/gens/tictactoe/BoardTest.java
@@ -26,7 +26,7 @@ public class BoardTest {
     	assertFalse(board.checkColWinner(table));
 	 }
 
-	 @Test
+	@Test
 	public void checkDiagonal() {
 
     	char[][] table = {{'X', '2', '3'}, {'X', 'X', '6'}, {'X', '8', 'X'}};
@@ -36,7 +36,7 @@ public class BoardTest {
     	assertTrue(board.checkColWinner(table));
 	 }
 
-     @Test
+    @Test
     public void isOverTest() {
         char[][] table = {{'1', '2', '3'}, {'4', '5', '6'}, {'7', '8', '9'}};
         char[][] table1 = {{'X', 'O', 'X'}, {'O', 'X', 'O'}, {'7', 'X', '9'}};
@@ -48,4 +48,13 @@ public class BoardTest {
         assertFalse(board.isOver(table1));
         assertTrue(board.isOver(table2));
     }
+
+    @Test 
+    public void canMoveTest() {
+        char[][] table = {{'O', 'X', 'X'}, {'X', 'X', 'O'}, {'O', 'O', 'X'}};
+        
+        Board board = new Board();
+
+        assertFalse(board.canMove(table));
+     }
 }
